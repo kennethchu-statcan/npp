99a1b247543244d6b5ce5b49e41752936d51350a
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-01-16 15:52:25 -0500

    updated: test-myCART.R

--- 008-pruning/code/test-myCART.R
+++ 008-pruning/code/test-myCART.R
@@ -8,6 +8,7 @@ test.myCART <- function(
     cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    require(rpart);
     data(iris);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
@@ -20,9 +21,52 @@ test.myCART <- function(
     cat("\nmyCART.object$print()\n");
     print( myCART.object$print()   );
 
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     list.pruning.sequence <- myCART.object$get_pruning_sequence();
     cat("\nstr(list.pruning.sequence)\n");
     print( str(list.pruning.sequence)   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    results.rpart <- rpart(
+        formula = Species ~ .,
+        data    = iris,
+        control = list(
+            minsplit  = 1,
+            minbucket = 1,
+            cp        = 0
+            )
+        );
+
+    cat("\nresults.rpart\n");
+    print( results.rpart   );
+
+    cat("\nstr(results.rpart)\n");
+    print( str(results.rpart)   );
+
+    cat("\nprintcp(results.rpart)\n");
+    printcp( results.rpart );
+
+    cat("\nresults.rpart[['cptable']]\n");
+    print( results.rpart[['cptable']]   );
+
+    cat("\nresults.rpart[['cptable']][,'CP']\n");
+    print( results.rpart[['cptable']][,'CP']   );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.temp <- rbind(
+        data.frame(
+            x = results.rpart[['cptable']][,'CP'],
+            y = rep(0,nrow(results.rpart[['cptable']]))
+            ),
+        data.frame(
+            x = as.numeric(lapply( X = list.pruning.sequence, FUN = function(x) {x[['alpha']]} )),
+            y = rep(1,length(list.pruning.sequence))
+            )
+        );
+
+    png('temp-cost-complexity.png');
+    plot( x = DF.temp[,'x'] , y = DF.temp[,'y'] );
+    dev.off();
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     cat(paste0("\n# ",thisFunctionName,"() quits."));
