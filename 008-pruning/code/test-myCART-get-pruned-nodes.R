cba9b9794fdc2d2dff61070e1d553dc081666572
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-02-07 15:58:28 -0500

    updated: get_pruned_nodes.R test-myCART-get-pruned-nodes.R

--- 008-pruning/code/test-myCART-get-pruned-nodes.R
+++ 008-pruning/code/test-myCART-get-pruned-nodes.R
@@ -31,12 +31,24 @@ test.myCART.get.pruned.nodes <- function(
     print( list.subtree.sequence   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    temp.nodes <- myCART.object$nodes;
-    for ( subtree.index in seq(2,length(list.subtree.sequence)) ) {
-        temp.nodes <- get_pruned_nodes(
-            input.nodes  = temp.nodes,
-            pruning.info = list.subtree.sequence[[subtree.index]]
-            );
+    # temp.nodes <- myCART.object$nodes;
+    # for ( subtree.index in seq(2,length(list.subtree.sequence)) ) {
+    #     temp.nodes <- get_pruned_nodes(
+    #         input.nodes  = temp.nodes,
+    #         pruning.info = list.subtree.sequence[[subtree.index]]
+    #         );
+    #     }
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    for ( index.subtree in seq(1,length(list.subtree.sequence)) ) {
+        cat("\n")
+        cat(paste0("\n### index.subtree: ",index.subtree,"\n"));
+        cat("\nlist.subtree.sequence[[index.subtree]][['alpha']]:\n");
+        print( list.subtree.sequence[[index.subtree]][['alpha']]    );
+        cat("\nlist.subtree.sequence[[index.subtree]][['nodes_pruned_at']]:\n");
+        print( list.subtree.sequence[[index.subtree]][['nodes_pruned_at']]    );
+        cat("\nprint_nodes(nodes = list.subtree.sequence[[index.subtree]][['pruned_nodes']])\n");
+        print_nodes(nodes = list.subtree.sequence[[index.subtree]][['pruned_nodes']]);
         }
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
