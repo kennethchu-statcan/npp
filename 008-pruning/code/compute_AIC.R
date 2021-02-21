bb460723e891b937aac16c1f6089c6adac8c7fe3
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-02-21 17:23:32 -0500

    added: compute_AIC.R

---
+++ 008-pruning/code/compute_AIC.R
@@ -0,0 +1,22 @@
+
+compute_AIC <- function(
+    DF.npdata.with.propensity = NULL,
+    DF.pdata.with.nodeID      = NULL
+    ) {
+
+    thisFunctionName <- "compute_AIC";
+
+    base::cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    base::cat(base::paste0("\n",thisFunctionName,"() starts.\n\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    base::require(survey);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    base::cat(base::paste0("\n# ",thisFunctionName,"() quits."));
+    base::cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    base::return( NULL );
+
+    }
