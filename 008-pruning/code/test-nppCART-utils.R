833a4f8e7591896e31c6a015fdea39eaef2603d5
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-03-14 15:15:04 -0400

    updated: test-nppCART-utils.R

--- 008-pruning/code/test-nppCART-utils.R
+++ 008-pruning/code/test-nppCART-utils.R
@@ -136,18 +136,18 @@ test.nppCART_get.population.02 <- function(
     require(survey);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    temp.centres <- c(0.5,1.5,2.5);
+    levels.x1 <- c("small","medium","large");
+    levels.x2 <- c("petit","moyen", "grand");
 
-    c1 <- sample(x = temp.centres, size = population.size, replace = TRUE);
-    c2 <- sample(x = temp.centres, size = population.size, replace = TRUE);
+    x1 <- factor(x = sample(x = levels.x1, size = population.size, replace = TRUE), levels = levels.x1, ordered = TRUE);
+    x2 <- factor(x = sample(x = levels.x2, size = population.size, replace = TRUE), levels = levels.x2, ordered = TRUE);
+
+    c1 <- as.numeric(x1) - 0.5;
+    c2 <- as.numeric(x2) - 0.5;
+    is.high.propensity <- (c2 - c1 == 1 | c2 - c1 == -1);
 
     true.propensity <- rnorm(n = population.size, mean = 0.25, sd = 0.025);
-    is.high.propensity <- (c2 - c1 == 1 | c2 - c1 == -1);
     true.propensity[is.high.propensity] <- rnorm(n = sum(is.high.propensity), mean = 0.75, sd = 0.025);
-
-    sigma <- 0.2; # 0.8;
-    x1 <- c1 + rnorm(n = population.size, mean = 0, sd = sigma);
-    x2 <- c2 + rnorm(n = population.size, mean = 0, sd = sigma);
 
     y0 <- rep(x = 30, times = population.size);
     y0[is.high.propensity] <- 110;
@@ -155,54 +155,19 @@ test.nppCART_get.population.02 <- function(
     epsilon <- rnorm(n = population.size, mean = 0, sd = 1.0)
     y <- y0 + epsilon^2;
 
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     DF.population <- data.frame(
         unit.ID         = seq(1,population.size),
         y               = y,
-        x1.numeric      = x1,
-        x2.numeric      = x2,
+        x1              = x1,
+        x2              = x2,
         true.propensity = true.propensity
         );
 
-    for ( colname.numeric in c("x1.numeric","x2.numeric") ) {
+    DF.population[,"x1.jitter"] <- as.numeric(DF.population[,"x1"]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
+    DF.population[,"x2.jitter"] <- as.numeric(DF.population[,"x2"]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
 
-        temp.quantiles <- quantile(
-            x     = DF.population[,colname.numeric],
-            probs = c(1,2,3)/3
-            );
-
-        is.small  <- ifelse(DF.population[,colname.numeric] <  temp.quantiles[1],TRUE,FALSE);
-        is.medium <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[1] & DF.population[,colname.numeric] < temp.quantiles[2],TRUE,FALSE);
-        is.large  <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[2],TRUE,FALSE);
-
-        colname.factor <- gsub(x = colname.numeric, pattern = "\\.numeric", replacement = "");
-        DF.population[,colname.factor] <- character(nrow(DF.population));
-
-        if ( "x1.numeric" == colname.numeric ) {
-            DF.population[is.small, colname.factor] <- "small";
-            DF.population[is.medium,colname.factor] <- "medium";
-            DF.population[is.large, colname.factor] <- "large";
-            temp.levels <- c("small","medium","large");
-        } else {
-            DF.population[is.small, colname.factor] <- "petit";
-            DF.population[is.medium,colname.factor] <- "moyen";
-            DF.population[is.large, colname.factor] <- "grand";
-            temp.levels <- c("petit","moyen","grand");
-            }
-
-        DF.population[,colname.factor] <- factor(
-            x       = DF.population[,colname.factor],
-            levels  = temp.levels,
-            ordered = TRUE
-            );
-
-        colname.jitter <- gsub(x = colname.numeric, pattern = "numeric", replacement = "jitter");
-        # DF.population[,colname.jitter] <- (-0.5) + as.numeric(DF.population[,colname.factor]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
-        DF.population[,colname.jitter] <- as.numeric(DF.population[,colname.factor]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
-
-        DF.population <- DF.population[,setdiff(colnames(DF.population),colname.numeric)];
-
-        }
-
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     return( DF.population );
 
     }
@@ -220,18 +185,18 @@ test.nppCART_get.population.03 <- function(
     require(survey);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    temp.centres <- c(0.5,1.5,2.5);
+    levels.x1.hidden <- c("small","medium","large");
+    levels.x2.hidden <- c("petit","moyen", "grand");
 
-    c1 <- sample(x = temp.centres, size = population.size, replace = TRUE);
-    c2 <- sample(x = temp.centres, size = population.size, replace = TRUE);
+    x1.hidden <- factor(x = sample(x = levels.x1.hidden, size = population.size, replace = TRUE), levels = levels.x1.hidden, ordered = TRUE);
+    x2.hidden <- factor(x = sample(x = levels.x2.hidden, size = population.size, replace = TRUE), levels = levels.x2.hidden, ordered = TRUE);
+
+    c1 <- as.numeric(x1.hidden) - 0.5;
+    c2 <- as.numeric(x2.hidden) - 0.5;
+    is.high.propensity <- (c2 - c1 == 1 | c2 - c1 == -1);
 
     true.propensity <- rnorm(n = population.size, mean = 0.25, sd = 0.025);
-    is.high.propensity <- (c2 - c1 == 1 | c2 - c1 == -1);
     true.propensity[is.high.propensity] <- rnorm(n = sum(is.high.propensity), mean = 0.75, sd = 0.025);
-
-    sigma <- 0.2; # 0.8;
-    x1 <- c1 + rnorm(n = population.size, mean = 0, sd = sigma);
-    x2 <- c2 + rnorm(n = population.size, mean = 0, sd = sigma);
 
     y0 <- rep(x = 30, times = population.size);
     y0[is.high.propensity] <- 110;
@@ -239,89 +204,40 @@ test.nppCART_get.population.03 <- function(
     epsilon <- rnorm(n = population.size, mean = 0, sd = 1.0)
     y <- y0 + epsilon^2;
 
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    levels.x1 <- as.vector(sapply( X = levels.x1.hidden, FUN = function(x) {return(paste(x,1:3,sep="."))} ));
+    levels.x2 <- as.vector(sapply( X = levels.x2.hidden, FUN = function(x) {return(paste(x,1:3,sep="."))} ));
+
     DF.population <- data.frame(
         unit.ID         = seq(1,population.size),
         y               = y,
-        x1.numeric      = x1,
-        x2.numeric      = x2,
+        x1.hidden       = x1.hidden,
+        x2.hidden       = x2.hidden,
+        subgroup.1      = sample(x = 1:3, size = population.size, replace = TRUE),
+        subgroup.2      = sample(x = 1:3, size = population.size, replace = TRUE),
         true.propensity = true.propensity
         );
 
-    for ( colname.numeric in c("x1.numeric","x2.numeric") ) {
+    DF.population[,'x1'] <- apply(
+        X      = DF.population[,c('x1.hidden','subgroup.1')],
+        MARGIN = 1,
+        FUN    = function(x) { return(paste(x,collapse=".")) }
+        );
 
-        temp.quantiles <- quantile(
-            x     = DF.population[,colname.numeric],
-            probs = seq(1,9)/9
-            );
+    DF.population[,'x2'] <- apply(
+        X      = DF.population[,c('x2.hidden','subgroup.2')],
+        MARGIN = 1,
+        FUN    = function(x) { return(paste(x,collapse=".")) }
+        );
 
-        is.small.1  <- ifelse(DF.population[,colname.numeric] <  temp.quantiles[1],TRUE,FALSE);
-        is.small.2  <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[1] & DF.population[,colname.numeric] < temp.quantiles[2],TRUE,FALSE);
-        is.small.3  <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[2] & DF.population[,colname.numeric] < temp.quantiles[3],TRUE,FALSE);
-        is.medium.1 <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[3] & DF.population[,colname.numeric] < temp.quantiles[4],TRUE,FALSE);
-        is.medium.2 <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[4] & DF.population[,colname.numeric] < temp.quantiles[5],TRUE,FALSE);
-        is.medium.3 <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[5] & DF.population[,colname.numeric] < temp.quantiles[6],TRUE,FALSE);
-        is.large.1  <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[6] & DF.population[,colname.numeric] < temp.quantiles[7],TRUE,FALSE);
-        is.large.2  <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[7] & DF.population[,colname.numeric] < temp.quantiles[8],TRUE,FALSE);
-        is.large.3  <- ifelse(DF.population[,colname.numeric] >= temp.quantiles[8],TRUE,FALSE);
+    DF.population[,'x1'] <- factor(x = DF.population[,'x1'], levels = levels.x1, ordered = TRUE);
+    DF.population[,'x2'] <- factor(x = DF.population[,'x2'], levels = levels.x2, ordered = TRUE);
 
-        colname.factor <- gsub(x = colname.numeric, pattern = "\\.numeric", replacement = "");
-        DF.population[,colname.factor] <- character(nrow(DF.population));
+    DF.population[,"x1.jitter"] <- as.numeric(DF.population[,"x1"]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
+    DF.population[,"x2.jitter"] <- as.numeric(DF.population[,"x2"]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
 
-        if ( "x1.numeric" == colname.numeric ) {
-
-            DF.population[is.small.1, colname.factor] <- "small.1";
-            DF.population[is.small.2, colname.factor] <- "small.2";
-            DF.population[is.small.3, colname.factor] <- "small.3";
-
-            DF.population[is.medium.1,colname.factor] <- "medium.1";
-            DF.population[is.medium.2,colname.factor] <- "medium.2";
-            DF.population[is.medium.3,colname.factor] <- "medium.3";
-
-            DF.population[is.large.1, colname.factor] <- "large.1";
-            DF.population[is.large.2, colname.factor] <- "large.2";
-            DF.population[is.large.3, colname.factor] <- "large.3";
-
-            temp.levels <- c(
-                "small.1", "small.2", "small.3",
-               "medium.1","medium.2","medium.3",
-                "large.1", "large.2", "large.3"
-               );
-
-        } else {
-
-            DF.population[is.small.1, colname.factor] <- "petit.1";
-            DF.population[is.small.2, colname.factor] <- "petit.2";
-            DF.population[is.small.3, colname.factor] <- "petit.3";
-
-            DF.population[is.medium.1,colname.factor] <- "moyen.1";
-            DF.population[is.medium.2,colname.factor] <- "moyen.2";
-            DF.population[is.medium.3,colname.factor] <- "moyen.3";
-
-            DF.population[is.large.1, colname.factor] <- "grand.1";
-            DF.population[is.large.2, colname.factor] <- "grand.2";
-            DF.population[is.large.3, colname.factor] <- "grand.3";
-
-            temp.levels <- c(
-               "petit.1","petit.2","petit.3",
-               "moyen.1","moyen.2","moyen.3",
-               "grand.1","grand.2","grand.3"
-               );
-
-            }
-
-        DF.population[,colname.factor] <- factor(
-            x       = DF.population[,colname.factor],
-            levels  = temp.levels,
-            ordered = TRUE
-            );
-
-        colname.jitter <- gsub(x = colname.numeric, pattern = "numeric", replacement = "jitter");
-        DF.population[,colname.jitter] <- as.numeric(DF.population[,colname.factor]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
-
-        DF.population <- DF.population[,setdiff(colnames(DF.population),colname.numeric)];
-
-        }
-
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    DF.population <- DF.population[,setdiff(colnames(DF.population),c('x1.hidden','x2.hidden','subgroup.1','subgroup.2'))];
     return( DF.population );
 
     }
