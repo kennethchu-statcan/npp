fbf224c1d7740ab3805f225620bf43f828a0b7b4
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-08-18 21:18:00 -0400

    updated: main-01-simulations.R

--- 011-categorical-independent/code/main-01-simulations.R
+++ 011-categorical-independent/code/main-01-simulations.R
@@ -50,7 +50,7 @@ for ( file.R in files.R ) {
 ##################################################
 is.macOS        <- grepl(x = sessionInfo()[['platform']], pattern = 'apple', ignore.case = TRUE);
 #population.size<- ifelse(test = is.macOS, yes = 5000, no = 40000); # 1000 ==> error
-population.size <- ifelse(test = is.macOS, yes = 5e5, no = 5e6); # 1000 ==> error
+population.size <- ifelse(test = is.macOS, yes = 5e5, no = 5e5); # 1000 ==> error
 n.simulations   <- ifelse(test = is.macOS, yes =  16, no = 200);
 prob.selection  <- as.numeric(pi/200); # as.numeric(pi/20); # 0.1570796,
 n.replicates    <- 500;
