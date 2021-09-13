a1633a073136434d7af4d77b780c22cf0df21ced
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2021-09-13 18:50:45 -0400

    updated: run-main.sh

--- 103-nppCART-test03/run-main.sh
+++ 103-nppCART-test03/run-main.sh
@@ -18,16 +18,16 @@ cp    $0         ${outputDIR}/code
 globalSeed=1234567
 
 ### ~~~~~ ###
-# myRscript=${codeDIR}/main-00-sanity.R
-# stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
-# stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
-# R --no-save --args ${codeDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
+myRscript=${codeDIR}/main-00-sanity.R
+stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
+stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
+R --no-save --args ${codeDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
 
 ### ~~~~~ ###
-# myPyScript=${codeDIR}/main-00-sanity-tree-hierarchy.py
-# stdoutFile=${outputDIR}/output-00-sanity-tree-hierarchy/stdout.py.`basename ${myPyScript} .py`
-# stderrFile=${outputDIR}/output-00-sanity-tree-hierarchy/stderr.py.`basename ${myPyScript} .py`
-# python ${myPyScript} ${codeDIR} ${outputDIR}/output-00-sanity-tree-hierarchy > ${stdoutFile} 2> ${stderrFile}
+myPyScript=${codeDIR}/main-00-sanity-tree-hierarchy.py
+stdoutFile=${outputDIR}/output-00-sanity-tree-hierarchy/stdout.py.`basename ${myPyScript} .py`
+stderrFile=${outputDIR}/output-00-sanity-tree-hierarchy/stderr.py.`basename ${myPyScript} .py`
+python ${myPyScript} ${codeDIR} ${outputDIR}/output-00-sanity-tree-hierarchy > ${stdoutFile} 2> ${stderrFile}
 
 ### ~~~~~ ###
 myRscript=${codeDIR}/main-01-simulations.R
