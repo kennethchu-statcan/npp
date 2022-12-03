0707b8f0fef4342566e290c192983852a0976574
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-12-03 18:28:51 -0500

    added: get-npp-samples.R get-synthetic-population.R nppCART-usage-with-pruning.Rmd nppCART-usage-with-pruning.html.asis

---
+++ 100-nppR-package/code/get-npp-samples.R
@@ -0,0 +1,125 @@
+#' Take a (non-probability, probability) pair of samples from the given synthetic population
+#'
+#' The \code{get.npp.samples} function draws a (non-probability, probability) pair of samples from the given synthetic population data frame (generated with \code{nppR::get.sythetic.population( )})
+#'
+#' @param DF.population data frame containing synthetic population data (generated with \code{nppR::get.synthetic.population( )})
+#' @param prob.selection numeric vector of length 1 containing the selection probability of the SRS probability sample. Must be a number strictly between 0 and 1. Default is 0.2.
+#' @param n.replicates numeric vector of length 1 containing the number of sets of bootstrap weights. Must be a positive integer. Default is 500.
+#' @param seed numeric vector of length 1 containing randomization seed. Must be a positive integer. Default is 7654321.
+#'
+#' @return list containing the non-probability and probability samples as data frames
+#'
+#' @examples
+#' DF.population <- nppR::get.synthetic.population(
+#'     population.size = 10000
+#'     );
+#'
+#' list.samples  <- nppR::get.npp.samples(
+#'     DF.population = DF.population,
+#'     prob.selection = 0.2,
+#'     n.replicates   = 500
+#'     );
+#'
+#' @importFrom stats formula lm coef nlm cor
+#' @export
+
+get.npp.samples <- function(
+    DF.population  = NULL,
+    prob.selection = 0.2,
+    n.replicates   = 500,
+    seed           = 7654321
+    ) {
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    base::stopifnot(
+        !base::is.null(DF.population), # must not be NULL
+        base::is.data.frame(DF.population) | base::is.matrix(DF.population) | tibble::is_tibble(DF.population), # must be matrix-like data type
+        base::nrow(DF.population) > 0, # must not be empty
+        base::all(base::c("unit.ID","y","x1","x2") %in% base::colnames(DF.population))
+        );
+
+    base::stopifnot(
+        !base::is.null(prob.selection), # must not be NULL
+        base::is.numeric(prob.selection), # must be numeric
+        (base::length(prob.selection) == 1), # must be lf length 1
+        (prob.selection > 0), # must be strictly positive
+        (prob.selection < 1) # must be strictly less than 1
+        );
+
+    base::stopifnot(
+        !base::is.null(n.replicates), # must not be NULL
+        base::is.numeric(n.replicates), # must be numeric
+        (base::length(n.replicates) == 1), # must be lf length 1
+        (n.replicates == as.integer(n.replicates)), # must be an integer
+        (n.replicates > 0) # must be strictly positive
+        );
+
+    base::stopifnot(
+        !base::is.null(seed), # must not be NULL
+        base::is.numeric(seed), # must be numeric
+        (base::length(seed) == 1), # must be lf length 1
+        (seed == as.integer(seed)), # must be an integer
+        (seed > 0) # must be strictly positive
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    base::set.seed(seed = seed);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  # colnames.np <- c("unit.ID","y","x1","x2","x3","x1.jitter","x2.jitter","x3.hidden");
+    colnames.np <- c("unit.ID","y","x1","x2","x3");
+    colnames.p  <- c("unit.ID","x1","x2","x3");
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    require(survey);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    is.self.selected   <- sapply(
+        X   = DF.population[,"true.propensity"],
+        FUN = function(x) { sample(x = c(FALSE,TRUE), size = 1, prob = c(1-x,x)) }
+        );
+    DF.non.probability <- DF.population;
+    DF.non.probability[,"self.selected"] <- is.self.selected;
+    DF.non.probability <- DF.non.probability[DF.non.probability[,"self.selected"],colnames.np];
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    is.selected <- sample(
+        x       = c(TRUE,FALSE),
+        size    = nrow(DF.population),
+        replace = TRUE,
+        prob    = c(prob.selection, 1 - prob.selection)
+        );
+
+    DF.probability <- DF.population[is.selected,colnames.p];
+    DF.probability[,"sampling.fraction"] <- prob.selection;
+    DF.probability[,"design.weight"    ] <- 1 / prob.selection;
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    my.svydesign.object <- survey::svydesign(
+        data = DF.probability,
+        id   = ~1,
+        fpc  = ~sampling.fraction
+        );
+
+    my.svrepdesign.object <- survey::as.svrepdesign(
+        design     = my.svydesign.object,
+        type       = "bootstrap",
+        replicates = n.replicates # 500
+        );
+
+    DF.repweights <- my.svrepdesign.object[['repweights']][['weights']];
+    DF.repweights <- as.data.frame(DF.repweights);
+    colnames(DF.repweights) <- paste0("repweight",seq(1,ncol(DF.repweights)));
+
+    DF.probability <- cbind(
+        my.svrepdesign.object[['variables']],
+        DF.repweights
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    return(list(
+        DF.non.probability = DF.non.probability,
+        DF.probability     = DF.probability
+        ));
+
+    }
---
+++ 100-nppR-package/code/get-synthetic-population.R
@@ -0,0 +1,127 @@
+#' Generate a synthetic population
+#'
+#' The \code{get.synthetic.population} function generate a synthetic population; it is used in the vignettes.
+#'
+#' @param population.size numeric vector of lengh 1 indicating desired population size. Must be a positive integer. Default value is 10000.
+#' @param seed numeric vector of length 1 containing randomization seed. Must be a positive integer. Default is 1234567.
+#'
+#' @return a data frame containing data of the synthetic population.
+#'
+#' @examples
+#' DF.population <- nppR::get.synthetic.population(
+#'     population.size = 10000
+#'     );
+#'
+#' @importFrom stats formula lm coef nlm cor
+#' @export
+
+get.synthetic.population <- function(
+    population.size = 10000,
+    seed            = 1234567
+    ) {
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    base::stopifnot(
+        !base::is.null(population.size), # must not be NULL
+        base::is.numeric(population.size), # must be numeric
+        (base::length(population.size) == 1), # must be lf length 1
+        (population.size == as.integer(population.size)), # must be an integer
+        (population.size > 0) # must be strictly positive
+        );
+
+    base::stopifnot(
+        !base::is.null(seed), # must not be NULL
+        base::is.numeric(seed), # must be numeric
+        (base::length(seed) == 1), # must be lf length 1
+        (seed == as.integer(seed)), # must be an integer
+        (seed > 0) # must be strictly positive
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    base::set.seed(seed = seed);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ordered.x1 <- TRUE;
+    ordered.x2 <- TRUE;
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    if ( ordered.x1 ) {
+        levels.x1.hidden <- base::c("small","medium","large");
+    } else {
+        levels.x1.hidden <- base::c("apple","orange","banana");
+        }
+
+    if ( ordered.x2 ) {
+        levels.x2.hidden <- base::c("petit","moyen", "grand");
+    } else {
+        levels.x2.hidden <- base::c("pomme","orange","banane");
+        }
+
+    levels.x3.hidden <- c("A","B","C");
+
+    x1.hidden <- factor(x = sample(x = levels.x1.hidden, size = population.size, replace = TRUE), levels = levels.x1.hidden, ordered = ordered.x1);
+    x2.hidden <- factor(x = sample(x = levels.x2.hidden, size = population.size, replace = TRUE), levels = levels.x2.hidden, ordered = ordered.x2);
+    x3.hidden <- factor(x = sample(x = levels.x3.hidden, size = population.size, replace = TRUE), levels = levels.x3.hidden, ordered = FALSE     );
+
+    c1 <- base::as.double(x1.hidden) - 0.5;
+    c2 <- base::as.double(x2.hidden) - 0.5;
+    c3 <- base::as.double(x3.hidden) - 0.5;
+    is.off.diagonals <- (c2 - c1 == 1 | c2 - c1 == -1);
+
+    is.high.propensity.A <- ( (x3.hidden == "A") &   is.off.diagonals  );
+    is.high.propensity.B <- ( (x3.hidden == "B") & (!is.off.diagonals) );
+    is.high.propensity.C <- ( (x3.hidden == "C") &   is.off.diagonals  );
+    is.high.propensity   <- ( is.high.propensity.A | is.high.propensity.B | is.high.propensity.C );
+
+    true.propensity                     <- rnorm(n = population.size,         mean = 0.20, sd = 0.015);
+    true.propensity[is.high.propensity] <- rnorm(n = sum(is.high.propensity), mean = 0.80, sd = 0.015);
+
+    y0 <- rep(x = 30, times = population.size);
+    y0[is.high.propensity] <- 110;
+
+    y <- y0 + rnorm(n = population.size, mean = 0, sd = 1.0)^2;
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    n.subgroups <- 2;
+    levels.x1   <- as.vector(sapply( X = levels.x1.hidden, FUN = function(x) {return(paste(x,1:n.subgroups,sep="."))} ));
+    levels.x2   <- as.vector(sapply( X = levels.x2.hidden, FUN = function(x) {return(paste(x,1:n.subgroups,sep="."))} ));
+
+    DF.population <- data.frame(
+        unit.ID         = seq(1,population.size),
+        y               = y,
+        x1.hidden       = x1.hidden,
+        x2.hidden       = x2.hidden,
+        subgroup.1      = sample(x = 1:n.subgroups, size = population.size, replace = TRUE),
+        subgroup.2      = sample(x = 1:n.subgroups, size = population.size, replace = TRUE),
+        x3              = x3.hidden,
+        x3.hidden       = factor(x = as.character(x3.hidden), levels = levels.x3.hidden, ordered = FALSE),
+        true.propensity = true.propensity
+        );
+
+    DF.population[,'x1'] <- apply(
+        X      = DF.population[,c('x1.hidden','subgroup.1')],
+        MARGIN = 1,
+        FUN    = function(x) { return(paste(x,collapse=".")) }
+        );
+
+    DF.population[,'x2'] <- apply(
+        X      = DF.population[,c('x2.hidden','subgroup.2')],
+        MARGIN = 1,
+        FUN    = function(x) { return(paste(x,collapse=".")) }
+        );
+
+    DF.population[,'x1'] <- factor(x = DF.population[,'x1'], levels = levels.x1, ordered = ordered.x1);
+    DF.population[,'x2'] <- factor(x = DF.population[,'x2'], levels = levels.x2, ordered = ordered.x2);
+
+    DF.population[,"x1.jitter"] <- as.double(DF.population[,"x1"]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
+    DF.population[,"x2.jitter"] <- as.double(DF.population[,"x2"]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
+
+  # DF.population[,"x3"] <- as.double(DF.population[,"x3"]) + runif(n = nrow(DF.population), min = -0.3, max = 0.3);
+    DF.population[,"x3"] <- as.double(DF.population[,"x3"]) + sample(x = c(-0.1,0.1), size = nrow(DF.population), replace = TRUE);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+  # DF.population <- DF.population[,c('unit.ID','y','x1','x2','x3','true.propensity','x1.jitter','x2.jitter','x3.hidden')];
+    DF.population <- DF.population[,c('unit.ID','y','x1','x2','x3','true.propensity')];
+    return( DF.population );
+
+    }
